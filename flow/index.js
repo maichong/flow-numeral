5083f80a4c5172973d552d4bc800918c653583b6
Liang Xingchen <liang@maichong.it>
2017-01-17 19:14:18 +0800

    0.1.1

--- flow/index.js
+++ flow/index.js
@@ -1,9 +1,16 @@
 declare module 'numeral' {
   declare class Numeral {
+    static (val: string|number|Numeral):Numeral;
     static defaultFormat(format: string):void;
     static zeroFormat(format: string):void;
     static nullFormat(format: string):void;
+    static reset():void;
     static register(type: string, sub: string, options: Object):void;
+    static isNumeral(val: any):boolean;
+    static version:string;
+    static options:{};
+    static formats:{};
+    static locales:{};
 
     constructor(input: number|string):void;
     format(format?: string):string;
@@ -13,6 +20,7 @@ declare module 'numeral' {
     multiply(val: number):void;
     divide(val: number):void;
     set(val: number):void;
+    difference(val: number|Numeral):number;
     clone():Numeral;
   }
 
